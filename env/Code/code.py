ef90f19eb9fad29fad0194e5d20a0ecc9222c70e
Ganesh Jainarain <RicheyJay2@gmail.com>
2022-11-09 16:28:37 -0500

    Added to read me

---
+++ env/Code/code.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+# Read in data into a dataframe 
+data = pd.read_csv('/Users/richeyjay/Desktop/MachineLearning/env/Code/Energy_and_Water_Data_Disclosure_for_Local_Law_84_2017__Data_for_Calendar_Year_2016_.csv')
+# Display top of dataframe
+print(data.head())
